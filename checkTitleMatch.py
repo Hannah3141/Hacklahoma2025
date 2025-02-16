9a1c913f28ce9de7517e44c9b4c46036ea606774
Hannah3141 <hannahmalini@gmail.com>
2025-02-15 21:12:59 -0600

    its a mess but i need it to save

--- checkTitleMatch.py
+++ checkTitleMatch.py
@@ -7,9 +7,14 @@ def get_book(title):
     response = requests.get(
         f"https://tccl.bibliocommons.com/v2/search?query={title[0]}&searchType=title"
     )
+    #<a data-key="bib-title" data-test-id="bib-title-S63C6527750" href="/item/show/6527750063" lang="en" rel="noopener" target="_parent">
     soup = BeautifulSoup(response.text, 'html.parser')
     first_result = soup.find('div', class_='cp-search-result-item-content')
+    if first_result == None:
+        return False
     title_elem = first_result.find('span', class_='title-content')
+    magic_number = first_result.find('a', data-key_='bib-title')
+    print(magic_number)
     if title_elem.contents == title:
           return True #list of libraries that have it
     else:
